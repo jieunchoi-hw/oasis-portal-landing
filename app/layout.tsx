252ecd55fa232b3381f2eab24f8117e8b1e189ee
choijieun <choijieun@hanwha.com>
2025-07-31 13:41:30 +0900

    폰트설정제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="bg-gray-50 font-inter tracking-tight text-gray-900 antialiased">
+      <body
+        className={`bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
+      >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
         </div>
